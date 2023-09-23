467e9a6f8b2a43ba38956629d8adac57b6eda80e
D7OOMIALSAGHEIR <abdulrahmanalsagheir1@gmail.com>
2023-09-23 22:14:42 +0300

    Implementation for Insertion sort

--- Sorter.java
+++ Sorter.java
@@ -1,3 +1,16 @@
 public class Sorter{
+    public static void insertionSort(int[] array){
+        for(int i = 1; i<array.length; i++){
+            int temp = array[i];
+            int j;
+            for(j = i-1; j >= 0 && temp < array[j]; j = j-1){
+                array[j+1] = array[j];
+            }
+            array[j+1] = temp;
+        }
+    }
     
+
+    
+
 }
