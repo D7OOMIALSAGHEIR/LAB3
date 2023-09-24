af374970e8a714d48df99e6957a1563b05281651
AbduLRaHMaNDS <Ashr.aldossary@gmail.com>
2023-09-24 13:39:25 +0300

    Selection sort added

--- Sorter.java
+++ Sorter.java
@@ -15,8 +15,26 @@ public class Sorter{
     public static void callInsertionSort(int[] arr){
         insertionSort(arr);
     }
+    // implementation of selection sort 
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+        }
+        public static void callSelectionSort(int[] arr){
+            selectionSort(arr);
+        }
+    }  
 
 
     
 
-}
